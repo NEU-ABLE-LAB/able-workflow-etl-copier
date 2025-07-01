3f3c6365e7d09f6c033a4361ffc23edb2a7c6848
thisismikekane <mi.kane@northeastern.edu>
2025-07-01 14:10:30 -0400

    Update ETL process and templates
    - Updated the reference in the GitHub Actions workflow to a new commit for the able-workflow-copier-dev repository.
    - Removed the default description from the ETL description field in copier.yml.
    - Changed the value of `requires_extract_external` in etl.yml from lowercase to uppercase.
    - Enhanced the Jinja template for the ETL process logs with detailed next steps and tasks.
    - Added new test files for the Snakemake rules and runner functions.
    - Created new modules for extract, load, and transform functionalities in the ETL process.
    - Implemented a new schema for validating the output of the ETL process.
    - Removed outdated or unnecessary files related to the ETL process.
    - Improved documentation and comments throughout the codebase for clarity and maintainability.

--- template/{{ package_name }}/{{ module_type }}/{{ etl_name }}/runner/main.py
+++
@@ -1,8 +0,0 @@
-"""
-This is the main entry point for the {{ etl_name_long }} ETL process.
-
-It orchestrates the execution of the ETL process by calling the
-`extract`, `transform`, and `load` modules in sequence. The process
-can be run from the command line or executed with Snakemake using the
-`{{ package_name }}_rules.py` script as part of a snakemake rule.
-"""
